dd9396314d8adf0b3fdaf95905c9d9171de4c3ba
Ali Aghdaei <85693952+aghdaei@users.noreply.github.com>
2022-06-08 23:58:41 -0400

    Update Run_experimen.jl

--- src/Run_experimen.jl
+++ src/Run_experimen.jl
@@ -10,12 +10,7 @@ include("HyperSF.jl")
 
 Input = "../data/ibm01.hgr"
 
-#Threshold on the conductance of the selected clusters; the lower, the better
-#should be less than 1
-CndT = 0.8
-
-# AC is the average conductance of all clusters
-AC, ar_coarse = HyperSF(Input, CndT)
+ar_coarse = HyperSF(Input, CndT)
 
 # The incidence matrix corresponding to the coarsened hypergraph
 H = INC3(ar_coarse)
