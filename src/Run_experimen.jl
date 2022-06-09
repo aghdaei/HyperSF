5a8c267c2617f0a1427a4d8b1e8776fcfb444084
Ali Aghdaei <85693952+aghdaei@users.noreply.github.com>
2022-06-09 00:17:11 -0400

    Update Run_experimen.jl

--- src/Run_experimen.jl
+++ src/Run_experimen.jl
@@ -1,10 +1,11 @@
+cd("/Users/aliaghdaei/Desktop/HyperSF-main/src")
 using SparseArrays
 using LinearAlgebra
 using Clustering
 using Distances
 using Metis
 using Laplacians
-
+using StatsBase
 
 include("HyperSF.jl")
 
@@ -13,10 +14,9 @@ Input = "../data/ibm01.hgr"
 ar_coarse = HyperSF(Input)
 
 # The incidence matrix corresponding to the coarsened hypergraph
-H = INC3(ar_coarse)
+H = INC(ar_coarse)
 
 
 ## Generate the output hypergraph in hMetis format
 # First line: #hyperedges, #nodes
 Whgr("Output.hgr", ar_coarse)
-
