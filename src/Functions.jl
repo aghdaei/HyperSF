89a600f1e611a8e052cd2a7bd30a2ad44737837a
Ali Aghdaei <85693952+aghdaei@users.noreply.github.com>
2022-08-22 14:58:30 -0400

    Update Functions.jl

--- src/Functions.jl
+++ src/Functions.jl
@@ -172,124 +172,6 @@ function HSC(ar, SV)
     return score
 end
 
-## Input: hypergraph array, a set of smoothed vectors
-# Output: a vector of node indices
-function HEC(ar, SV)
-
-    mx = mxF(ar)
-
-    ratio = 2
-
-    val = 0
-
-    M = length(ar)
-
-    EL = collect(UnitRange(1,M))
-
-    idx_new = zeros(Int, mx)
-
-    new_NL = Any[]
-
-    new_NN = Any[]
-
-    rem_vec = []
-
-    flag = falses(1, mx)
-
-    ## node clustering
-
-    ss = zeros(Int, 0)
-
-    for i = 1:length(ar)
-
-        append!(ss, length(ar[i]))
-
-    end
-
-    highE = sort(ss, rev = true)
-
-    fd1 = findall(x->x > 0, highE)
-
-    Epos = sortperm(ss, rev=true)
-
-    E = Epos[fd1]
-
-
-    for iter2 = 1:length(E)
-
-        nd = ar[E[iter2]]
-
-        fd_flag = flag[nd]
-
-        node = nd[.!fd_flag]
-
-
-        if length(node) > 0
-
-            sm_mat = view(SV, node, :)
-
-            R = kmeans(sm_mat', ceil(Int, length(node)/ratio));
-
-            idx_clus = R.assignments
-
-            ############ Counting the size of each clustering
-            string_count = StatsBase.countmap(idx_clus)
-
-            ks = collect(keys(string_count))
-
-            vls = collect(values(string_count))
-
-            fd1 = findall(x->x==1, vls)
-
-            ############# end of counting cluster size
-
-            ### removing the single nodes
-            fd2 = findall(x-> in(x, ks[fd1]), idx_clus)
-
-            deleteat!(idx_clus, fd2)
-
-            deleteat!(node, fd2)
-            ############ end of removing
-
-            if length(idx_clus) > 0
-
-                ### rearranging the cluster indices
-                val3 = 1
-
-                idx_clus2 = zeros(Int, length(idx_clus))
-
-                UN = unique(idx_clus)
-
-                for jj = 1:length(UN)
-
-                    fd3 = findall(x->x==UN[jj], idx_clus)
-
-                    idx_clus2[fd3] .= val3
-
-                    val3 += 1
-
-                end # for jj
-
-                #### end of rearranging the cluster indices
-
-                idx_new[node] = idx_clus2 .+ val
-
-                flag[node] .= 1
-
-                val = val + maximum(idx_clus2)
-
-            end # end if length of the cluster
-
-        end # end if length(node) > 1
-
-
-    end # end of iter2
-
-    return idx_new
-
-end#function
-
-
 ## Input: hypergraph array
 # Output: an array showing the hyperedges belong to each node
 function HyperNodes(ar)
@@ -314,6 +196,195 @@ function HyperNodes(ar)
 
     return NH1
 
+end
+
+## Input: hypergraph array, levels of coarsening using k-mean clustering
+# Output: the cluster indices of every node
+function decomposition(ar, L)
+
+    ar_new = Any[]
+
+    idx_mat = Any[]
+
+    Neff = zeros(Float64, mxF(ar))
+
+    W = ones(Float64, length(ar))
+
+    @time @inbounds for loop = 1:L
+
+        mx = mxF(ar)
+
+        ## star expansion
+        A = StarW(ar, W)
+
+        ## computing the smoothed vectors
+        initial = 0
+
+        SmS = 100
+
+        interval = 20
+
+        Nrv = 1
+
+        RedR = 1
+
+        Nsm = Int((SmS - initial) / interval)
+
+        Ntot = Nrv * Nsm
+
+        Qvec = zeros(Float64, 0)
+
+        Eratio = zeros(Float64, length(ar), Ntot)
+
+        global SV = zeros(Float64, mx, Ntot)
+
+        for ii = 1:Nrv
+
+            sm = zeros(mx, Nsm)
+
+            Random.seed!(1); randstring()
+
+            rv = (rand(Float64, size(A, 1), 1) .- 0.5).*2
+
+            sm = Filter(rv, SmS, A, mx, initial, interval, Nsm)
+
+            SV[:, (ii-1)*Nsm+1 : ii*Nsm] = sm
+
+        end
+
+        ## Make all the smoothed vectors orthogonal to each other
+        QR = qr(SV)
+
+        SV = Matrix(QR.Q)
+
+        ## Computing the ratios using all the smoothed vectors
+        for jj = 1:size(SV, 2)
+
+            hscore = HSC(ar, SV[:, jj])
+
+            Eratio[:, jj] = hscore ./ sum(hscore)
+
+        end #for jj
+
+        ## Approximating the effective resistance of hyperedges by selecting the top ratio
+        #global Evec = sum(Eratio, dims=2) ./ size(SV,2)
+        E2 = sort(Eratio, dims=2, rev=true)
+        Evec = E2[:, 1]
+
+        # Adding the effective resistance of super nodes from previous levels
+        @inbounds for kk = 1:length(ar)
+
+            nd2 = ar[kk]
+
+            Evec[kk] = Evec[kk] + sum(Neff[nd2])
+
+        end
+
+        ## Normalizing the ERs
+        P = Evec ./ maximum(Evec)
+
+        ## Choosing a ratio of all the hyperedges
+        Nsample = round(Int, RedR * length(ar))
+
+        PosP = sortperm(P[:,1])
+
+        ## Increasing the weight of the hyperedges with small ERs
+        W[PosP[1:Nsample]] = W[PosP[1:Nsample]] .* (1 .+  1 ./ P[PosP[1:Nsample]])
+
+        ## Selecting the hyperedges with higher weights for contraction
+        Pos = sortperm(W, rev=true)
+        #global Pos = [4,3,2,1]
+
+        ## Hyperedge contraction
+        flag = falses(mx)
+
+        flagE = falses(length(ar))
+
+        val = 1
+
+        idx = zeros(Int, mx)
+
+        Neff_new = zeros(Float64, 0)
+
+        @inbounds for ii = 1:Nsample
+
+            nd = ar[Pos[ii]]
+
+            fg = flag[nd]
+
+            fd1 = findall(x->x==0, fg)
+
+            if length(fd1) > 1
+
+                nd = nd[fd1]
+
+                flagE[Pos[ii]] = 1
+
+                idx[nd] .= val
+
+                flag[nd] .= 1
+
+                val +=1
+
+                ## creating the super node weights
+                new_val = Evec[Pos[ii]] + sum(Neff[nd])
+
+                append!(Neff_new, new_val)
+
+            end # endof if
+
+        end #end of for ii
+
+        ## indexing the isolated nodes
+        fdz = findall(x-> x==0, idx)
+
+        fdnz = findall(x-> x!=0, idx)
+
+        V = vec(val:val+length(fdz)-1)
+
+        idx[fdz] = V
+        ## Adding the weight od isolated nodes
+        append!(Neff_new, Neff[fdz])
+
+        push!(idx_mat, idx)
+
+        ## generating the coarse hypergraph
+        ar_new = Any[]
+
+        @inbounds for ii = 1:length(ar)
+
+            nd = ar[ii]
+
+            nd_new = unique(idx[nd])
+
+            push!(ar_new, sort(nd_new))
+
+        end #end of for ii
+
+        ## Keeping the edge weights of non unique elements
+        fdnu = unique(z -> ar_new[z], 1:length(ar_new))
+        W2 = W[fdnu]
+
+
+        ## removing the repeated hyperedges
+        ar_new = unique(ar_new)
+
+        ### removing hyperedges with cardinality of 1
+        HH = INC(ar_new)
+        ss = sum(HH, dims=2)
+        fd1 = findall(x->x==1, ss[:,1])
+        deleteat!(ar_new, fd1)
+        deleteat!(W2,fd1)
+
+        ar = ar_new
+
+        Neff = Neff_new
+
+        W = W2
+
+    end #end for loop
+
+    return ar, idx_mat, SV
 end
 
 
