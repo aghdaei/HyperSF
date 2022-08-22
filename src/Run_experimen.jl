8114a57b31ecb8d9d88d9a96be23347188cfaca3
Ali Aghdaei <85693952+aghdaei@users.noreply.github.com>
2022-08-22 15:51:46 -0400

    Update Run_experimen.jl

--- src/Run_experimen.jl
+++ src/Run_experimen.jl
@@ -29,3 +29,28 @@ R = .1
 ## IDN is the index cluster that is assigned to each node
 IDN = HyperSF(filename, L, R)
 
+
+
+## Writing the coarse hypergraph into output file
+cd("../data/")
+ar = ReadInp(filename)
+cd("../src/")
+ar_new = Any[]
+@inbounds for ii = 1:length(ar)
+    
+    nd = ar[ii]
+    ndN = unique(IDN[nd])
+    push!(ar_new, sort(ndN))
+
+end #end of for ii
+
+ar_new = unique(ar_new)
+
+### removing hyperedges with cardinality of 1
+HH = INC(ar_new)
+ss = sum(HH, dims=2)
+fd1 = findall(x->x==1, ss[:,1])
+deleteat!(ar_new, fd1)
+
+
+Whgr("Out.hgr", ar_new)
